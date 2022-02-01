8a4e1afcce0b1a2fb8c49120350ea3583c7b96c3
mr.Escow <escowxbox@gmail.com>
2022-02-01 04:53:20 +0300

    Create Decorator BombDecorator

--- SBomberProject/Bomb.cpp
+++ SBomberProject/Bomb.cpp
@@ -4,6 +4,7 @@
 #include "Bomb.h"
 #include "ScreenSingletone.h"
 
+
 using namespace std;
 
 void Bomb::Draw() const
@@ -11,4 +12,17 @@ void Bomb::Draw() const
     ScreenSingleton::getInstance().SetColor(CC_LightMagenta);
     ScreenSingleton::getInstance().GotoXY(x, y);
     cout << "*";
+}
+
+void BombDecorator::Move(uint16_t time)
+{
+    bomb.Move(time * 1.6);
+}
+
+void BombDecorator::Draw() const
+{
+    bomb.Draw();
+    // Некоторое изменение внешнего вида бомбы
+    ScreenSingleton::getInstance().GotoXY(bomb.GetX(), bomb.GetY() - 1);
+    cout << "|";
 }
--- SBomberProject/Bomb.h
+++ SBomberProject/Bomb.h
@@ -14,3 +14,18 @@ private:
 
 };
 
+class BombDecorator : public DynamicObject
+{
+private:
+	Bomb bomb;
+public:
+	void Draw() const override;
+	void Move(uint16_t time) override;
+
+	void SetPos(double nx, double ny) override { bomb.SetPos(nx, ny); }
+	double GetY() const override { return bomb.GetY(); }
+	double GetX() const override { return bomb.GetX(); }
+	void SetWidth(uint16_t widthN) override { bomb.SetWidth(widthN); }
+	uint16_t GetWidth() const override { bomb.GetWidth(); }
+
+};
