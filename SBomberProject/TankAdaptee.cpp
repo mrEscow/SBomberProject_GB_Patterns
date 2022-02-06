b7148d968e243b2165c61e08c55fa40a7ce4ad7b
mr.Escow <escowxbox@gmail.com>
2022-02-06 07:15:53 +0300

    Create Adaper Pattern

--- SBomberProject/TankAdaptee.cpp
+++ SBomberProject/TankAdaptee.cpp
@@ -1,5 +1,42 @@
+#include <iostream>
 #include "TankAdaptee.h"
+#include "MyTools.h"
+
+using namespace std;
+using namespace MyTools;
 
 void TankAdaptee::Paint()
 {
+	MyTools::SetColor(CC_Brown);
+	GotoXY(x, y - 3);
+	cout << "    #####";
+	GotoXY(x - 2, y - 2);
+	cout << "#######   #";
+	GotoXY(x, y - 1);
+	cout << "    #####";
+	GotoXY(x, y);
+	cout << " ###########";
+}
+
+bool __fastcall TankAdaptee::isInRange(double x1, double x2) const
+{
+	const double XBeg = x + 2;
+	const double XEnd = x + width - 1;
+
+	if (x1 < XBeg && x2 > XEnd)
+	{
+		return true;
+	}
+
+	if (x1 > XBeg && x1 < XEnd)
+	{
+		return true;
+	}
+
+	if (x2 > XBeg && x2 < XEnd)
+	{
+		return true;
+	}
+
+	return false;
 }
